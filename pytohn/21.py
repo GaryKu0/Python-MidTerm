4018c71d7e406841d3b7a42ff1c068b566a23582
GaryKu0 <42890979+GaryKu0@users.noreply.github.com>
2022-03-23 16:10:14 +0800

    學生資料搜尋程式

---
+++ pytohn/21.py
@@ -0,0 +1,3 @@
+Student_Data={"123":["Tom","DTGD"],"456":["Cat","CSIE"],"789":["Nana","ASIE"],"321":["Lim","DBA"],"654":["Won","FDD"]}
+Search=input("輸入學號:")
+print("學生學號:",Search,"名字:",Student_Data[Search][0],"系別:",Student_Data[Search][1])
