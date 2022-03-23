611a1e6dfaaa58fa6bf8545fa045afeb8ee5ce15
GaryKu0 <42890979+GaryKu0@users.noreply.github.com>
2022-03-23 17:03:10 +0800

    none

---
+++ pytohn/23.py
@@ -0,0 +1,6 @@
+inputs = int(input("輸入:"))
+while input!=-1:
+    answer=pow(inputs,3)/3
+    print(round(answer,1))
+    inputs = int(input("輸入:"))
+print("End")
---
+++ pytohn/24.py
@@ -0,0 +1,25 @@
+from gettext import find
+
+
+matrix=input("請輸入陣列大小:")
+Matrix = [[0 for col in range(int(matrix))] for row in range(int(matrix))]
+AllNum=[]
+for i in range(int(matrix)):
+    Matrix[i]=input("輸入Matrix為:").split(" ")
+    for j in range(int(matrix)):
+        AllNum.append(int(Matrix[i][j]))
+AllNum.sort(reverse=True)
+#print(AllNum)
+coordinate=[]
+Max=int(AllNum[0])+int(AllNum[1])+int(AllNum[2])
+for i in range(3):
+    Find=AllNum[i]
+    for i in range(len(Matrix)):
+        for j in range(len(Matrix[i])):
+            #print("目前搜尋的數字為:",Find,"被搜尋",Matrix[i][j])
+            if Matrix[i][j]==str(Find):
+                #print("位置為",i+1,",",j+1)
+                coordinate.append(str(i+1)+","+str(j+1))
+                break
+print(str(AllNum[0])+"+"+str(AllNum[1])+"+"+str(AllNum[2])+"="+str(Max))
+print(coordinate)
